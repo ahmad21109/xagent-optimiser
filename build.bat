e2fb7fc3f5ac42e48be71c8c7c344abff1093072
abdullah21109 <abdullahahmad21109@gmail.com>
2025-05-14 17:31:28 +0530

    Your commit message

--- build.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-pyinstaller --noconsole --onefile --uac-admin --name "xagent_optimiser_v1.0.5" --icon "D:\AHMAD ABDULLAH\Desktop\Important files\OPTIMISER\ppg.ico" --add-data "D:\AHMAD ABDULLAH\Desktop\Important files\OPTIMISER\version.json:." --add-data "D:\AHMAD ABDULLAH\Desktop\Important files\OPTIMISER\OPTI_UI.ui:." "D:\AHMAD ABDULLAH\Desktop\Important files\OPTIMISER\main.py"
-pause
